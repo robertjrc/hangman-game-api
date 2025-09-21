f02850abb9f4e4aa667d817d5fc1dabb9d8bbc58
Robert <gh.robert99@gmail.com>
2025-09-21 14:31:24 -0300

    refactor(newWord): new method name.

--- src/application/usecases/group/groupNewWordUsecase.js
+++ src/application/usecases/group/groupNewWordUsecase.js
@@ -1,13 +1,12 @@
 import { Group } from "../../../domain/entities/Group.js";
-import { Result } from "../../common/result.js";
 
-export class GroupSetWordUsecase {
+export class GroupNewWordUsecase {
     execute(group) {
         const newGroup = new Group(group.id, group.name);
 
         newGroup.setCurrentInfo(group.words);
         newGroup.current_info.message_id = group.current_info.message_id;
 
-        return Result.success("Question successfully defined.", newGroup.current_info);
+        return newGroup.current_info;
     }
 }
